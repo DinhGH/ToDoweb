7e08eb31a1935ed0d10deac08e75d783a7b32731
DinhGH <huynhdinh2k52707@gmail.com>
2025-06-24 11:15:12 +0700

    build again

--- src/app/api/deleteTask/[id]/route.ts
+++ src/app/api/deleteTask/[id]/route.ts
@@ -1,15 +1,13 @@
 import { prisma } from "@/app/lib/prisma";
+
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const id = Number(params.id);
+export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+  const id = context.params.id;
 
   try {
     await prisma.task.delete({
-      where: { id },
+      where: { id: Number(id) },
     });
 
     return NextResponse.json({ message: "Xoá thành công" });
