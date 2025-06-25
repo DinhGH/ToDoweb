59ba756db0f37651b9f4bb7251550391c5071535
DinhGH <huynhdinh2k52707@gmail.com>
2025-06-25 22:52:38 +0700

    build and deploy

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    ignoreBuildErrors: true, 
+    ignoreBuildErrors: true,
   },
+ 
 };
 
 export default nextConfig;
